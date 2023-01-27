60dbc3b09d2c46c083b604d13b7b90ebbf68c741
Fernando <fernandoalcocer812@hotmail.com>
2023-01-27 12:35:56 -0300

    Avanzando con los hooks useState, useEffect

---
+++ 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,21 @@
+import { useCounter } from "../hooks/useCounter";
+
+export const CounterWithCustomHook = () => {
+  const { counter, increment, decrement, reset } = useCounter();
+  return (
+    <>
+      <h1>Counter with Hook: {counter}</h1>
+      <hr />
+
+      <button onClick={() => increment(5)} className="btn btn-primary">
+        +5
+      </button>
+      <button onClick={reset} className="btn btn-primary">
+        reset
+      </button>
+      <button onClick={() => decrement(1)} className="btn btn-primary">
+        -1
+      </button>
+    </>
+  );
+};
---
+++ 05-hook-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <FormWithCustomHook />
+  // {/* </React.StrictMode> */}
+);
