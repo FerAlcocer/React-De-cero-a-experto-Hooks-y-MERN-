a511abd4e7c7fd1849b80cd21fe6b1d8e1b714b9
Fernando <fernandoalcocer812@hotmail.com>
2023-02-27 14:37:26 -0300

    Inicio de pruebas test heroes App

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,13 +1,21 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate("/", {
+    const lastPath = localStorage.getItem("lastPath") || "/";
+
+    login("Fernando Alcocer");
+
+    navigate(lastPath, {
       replace: true,
     });
   };
+
   return (
     <div className="container mt-5">
       <h1>Login</h1>
